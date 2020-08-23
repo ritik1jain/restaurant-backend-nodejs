87b89291ccb349714f3e691f550d36a1cf39e2bf
ritik1jain <ritikjain0706@gmail.com>
2020-08-23 19:38:12 +0530

    facebbok Oauth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey' : '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '306672327078296',
+        clientSecret: '4f46f60158421924441957a77449e910'
+    }
 }
